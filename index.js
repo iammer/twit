8a1ea6b8d02f4ebdfd27b69e5e44e31978cc6c52
Michael Ratliff <email@michaelratliff.com>
2017-03-17 18:31:40 -0400

    Add websockets

--- index.js
+++ index.js
@@ -15,18 +15,40 @@ app.use('/js', express.static(__dirname + '/public'));
 app.use('/css', express.static(__dirname + '/public'));
 
 app.get('/', (req, resp) => {
-    resp.sendFile(__dirname + '/public/index.html');
+    resp.sendFile(__dirname + '/public/login.html');
 });
 
 app.get('/:name', (req, resp) => {
-    resp.sendFile(__dirname + '/public/index.html');
+    const name = req.params.name;
+    if (name.length <= 3) {
+        console.log(`${name} has logged in`);
+        resp.sendFile(__dirname + '/public/index.html');
+    } else {
+        resp.sendFile(__dirname + '/public/login.html');
+    }
 });
 
-io.on('connection', socket =>
-    socket.join('room', () =>
-        socket.on('message', msg => socket.to('room', msg))
-    )
-);
+const msgs = [];
+io.on('connection', socket => {
+    socket.on('message', msg => {
+        console.log(msg);
+        if (msg.from.length <= 3 && msg.msg.length <= 11) {
+            const validMsg = {
+                from: msg.from,
+                msg: msg.msg,
+                time: Date.now()
+            };
+            msgs.push(validMsg);
+            io.emit('message', validMsg);
+        }
+    });
+
+    socket.on('ready', () => {
+        socket.emit('initial', msgs.slice(-20));
+    });
+});
+
+//setInterval(() => io.emit('message',{from: 'me', msg: 'Test'}), 1000);
 
 server.listen(8080);
 console.log('Started');

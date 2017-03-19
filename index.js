f0737c3e0384ffb79e1a007495b9faccb2742e56
Michael Ratliff <email@michaelratliff.com>
2017-03-19 18:51:28 +0000

    Use SSL

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 #!/usr/bin/env node
 
 const express = require('express');
-const http = require('http');
+const https = require('https');
 const socketIO = require('socket.io')
 const morgan = require('morgan');
 const fs = require('fs');
 const readline = require('readline'); 
 
+const options = {
+	key: fs.readFileSync(__dirname + '/certs/twit.key'),
+	cert: fs.readFileSync(__dirname + '/certs/twit.cer')
+};
+
 const app = express();
-const server = http.createServer(app);
+const server = https.createServer(options, app);
 const io = socketIO(server);
 
 app.use(morgan('combined'));
 
+app.use((req, resp, next) => {
+    if (req.hostname !== 'twit.iammer.com') {
+	console.log('Invalid hostname: ' + req.hostname);
+        resp.sendStatus(404);
+    } else {
+        next();
+    }
+});
+
 app.use('/js', express.static(__dirname + '/public'));
 app.use('/css', express.static(__dirname + '/public'));
+app.use('/.well-known', express.static(__dirname + '/public/.well-known'));
 
 app.get('/', (req, resp) => {
     resp.sendFile(__dirname + '/public/login.html');
@@ -61,7 +76,19 @@ io.on('connect', socket => {
     });
 });
 
-server.listen(8080);
+const http = require('http');
+
+http.createServer((req,resp) => {
+   if (req.headers.host !== 'twit.iammer.com') {
+        resp.writeHead(404);
+	resp.end();
+   } else {
+        resp.writeHead(302, { Location: 'https://twit.iammer.com'});
+	resp.end();
+   }
+}).listen(80);
+
+server.listen(443);
 console.log('Started');
 
 
